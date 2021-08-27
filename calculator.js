724e19e8b4a199eda66f80bff0d2a5883d2331a4
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-08-27 23:34:18 +0100

    Fixed bugs with power button and introduced calculations for powers

--- calculator.js
+++ calculator.js
@@ -11,38 +11,49 @@ const power = (a, b) => a**b;
 
 const numbers = document.querySelectorAll('[data-number]');
 const input = document.querySelector('#input');
-const output = document.querySelector('#equation')
+const output = document.querySelector('#equation');
 const operators = document.querySelectorAll('[data-operator]');
 const equalSign = document.querySelector(['[data-result]']);
 const decimalPoint = document.querySelector('[data-decimal]');
 const powerButton = document.querySelector('[data-power]');
-console.log(powerButton);
 let operation;
 const equation = [];
+// Controls when numbers are inputted as a power (e.g. x^y when true, y when false).
+let powerNumbers = false;
 
 // function to use the operators
 const operate = (operator, a, b) => operator(a, b);
 
-// function to display numbers on the calculator screen
 const displayNumber = () => {
     numbers.forEach(number => {
         number.addEventListener('click', (event) =>{
-            // if statment resets the calculator if a number is pressed after the equals sign is pressed
-            if (equation.length === 1){
-                input.textContent = `${number.dataset.number}`;
-                equation.splice(0, 1);
-                output.textContent = '';
-            }
-            else {
-                input.textContent += `${number.dataset.number}`;
+            if (powerNumbers === false){
+                // if statment resets the calculator if a number is pressed after the equals sign is pressed
+                if (equation.length === 1){
+                    input.textContent = `${number.dataset.number}`;
+                    equation.splice(0, 1);
+                    output.textContent = '';
+                }
+                else {
+                    input.textContent += `${number.dataset.number}`;
+                }
             }
         })
     })
+    
 }
 
 const operatorsDisplay = () => {
     operators.forEach(operator => {
         operator.addEventListener('click', (event) => {
+            
+            // Removes power if user clicks an operator without giving the power any values.
+            const power = document.querySelector('#powerDisplay');
+            const powerSymbol  = document.querySelector('#powerSymbol');
+            if ( powerNumbers === true && (!power.textContent)){
+                power.remove(); powerSymbol.remove();
+            }
+            
             // Adds operands and operators to equations array when there is nothing on the display.
             if (equation.length === 0)
             {
@@ -52,6 +63,7 @@ const operatorsDisplay = () => {
                 equation.push(event.target.dataset.operator);
                 input.textContent += ` ${operator.dataset.operator} `;
             }
+            
 
             // Checks for the index of the last operator added to the equations array and pushes the next number into the array.
             // This allows the user to perform multiple operations.
@@ -68,13 +80,20 @@ const operatorsDisplay = () => {
                 }
             }
 
+            if ( equation.length === 1 && powerNumbers === true){
+                equation[0] = input.textContent;
+            }
+
             if (equation.length === 1){
                 output.textContent = parseFloat(parseFloat(equation[0]).toFixed(4)).toString();
                 equation.push(event.target.dataset.operator);
                 input.textContent += ` ${event.target.dataset.operator} `;
                 operation = event.target.dataset.operator;
+                console.log(equation);
             }
             decimalPoint.disabled = false;
+            powerNumbers = false;
+            powerButton.disabled = false;
         })
     })
 
@@ -87,15 +106,26 @@ const decimals = () => {
             input.textContent = '0';
             equation.splice(0, 1);
             output.textContent = '';
+            input.textContent += '.';
+        }
+
+        if (powerNumbers === true){
+            const power = document.querySelector('#powerDisplay');
+            if (power.textContent.length === 0){
+                power.textContent = '0';
+            }
+
+            power.textContent += '.';
         }
 
         else if (input.textContent[input.textContent.length - 1] === " "){
             input.textContent += '0';
+            input.textContent += '.';
         }
-        input.textContent += '.'
         decimalPoint.disabled = true;
     })
 }
+
 
 const equals = () => {
     equalSign.addEventListener('click', (event) => {
@@ -108,6 +138,20 @@ const equals = () => {
             let result;
             // Variable checks if there is a multiplication or division symbol in the equation
             let multOrDiv = true; 
+            console.log(equation);
+
+            // Goes through the equations array an checks for any ^ so powers can be solved first
+            for (let i in equation){
+                if (equation[i].includes('^')){
+                    const caratIndex = equation[i].indexOf('^');
+                    const base = equation[i].slice(0, caratIndex);
+                    const exponent = equation[i].slice(caratIndex + 1);
+                    result = operate(power, parseFloat(base), parseFloat(exponent));
+                    result = result.toString();
+                    equation.splice(i, 1, result);
+                }
+                console.log(i);
+            }
 
             // Solves the calculation in the equations array according to operator precedence
             while (equation.length !== 1){
@@ -144,24 +188,48 @@ const equals = () => {
             output.textContent = input.textContent + ' =';
             //Sets decimals to 4 decimal places and deals with floating point errors.
             input.textContent = parseFloat(parseFloat(equation[0]).toFixed(4)).toString();
+
             decimalPoint.disabled = false;
+            powerButton.disabled = false;
+            powerNumbers = false;
         }
         })
 }
 
 const powerDisplay = () => {
     powerButton.addEventListener('click', (event) => {
-        const powerTag = document.querySelector('#powerDisplay');
-        console.log(powerTag);
-        powerTag.textContent = '22';
-        input.firstChild.nodeValue = '3';
+        if ( (!input.textContent) || input.textContent[input.textContent.length - 1] === ' '){
+            input.textContent += '0';
+        }
+        const powerSymbol = document.createElement('span');
+        const power = document.createElement('span');
+        powerSymbol.id = 'powerSymbol';
+        powerSymbol.textContent = '^';
+        power.id = 'powerDisplay';
+        input.appendChild(powerSymbol);
+        input.appendChild(power);
+        powerNumbers = true;
+        addPower(power);
+        
+        powerButton.disabled = true;
+    })
 
+}
+
+const addPower = (span) => {
+    numbers.forEach(number => {
+        number.addEventListener('click', (event) => {
+            if (powerNumbers === true){
+                span.textContent += event.target.dataset.number;
+            }
+        })
+        
     })
 }
 
 
-
 const solveEquation = () => {
+    
     displayNumber();
     decimals();
     operatorsDisplay();
