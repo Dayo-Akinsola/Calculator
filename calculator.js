544cd8eabfbe9bc5aade8fd455a1f1d71405ed48
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-08-29 23:36:45 +0100

    Added calculations for two cases involving square and cube root

--- calculator.js
+++ calculator.js
@@ -65,13 +65,13 @@ const displayNumber = () => {
             }
         })
     })
-    
+
 }
 
 const operatorsDisplay = () => {
     operators.forEach(operator => {
         operator.addEventListener('click', (event) => {
-            
+
             // Removes power if user clicks an operator without giving the power any values.
             const power = document.querySelector('#powerDisplay');
             const powerSymbol  = document.querySelector('#powerSymbol');
@@ -85,7 +85,7 @@ const operatorsDisplay = () => {
                     input.textContent += '0';
                 }
             })
-            
+
             // Adds operands and operators to equations array when there is nothing on the display.
             if (equation.length === 0)
             {
@@ -95,7 +95,7 @@ const operatorsDisplay = () => {
                 equation.push(event.target.dataset.operator);
                 input.textContent += ` ${operator.dataset.operator} `;
             }
-            
+
 
             // Checks for the index of the last operator added to the equations array and pushes the next number into the array.
             // This allows the user to perform multiple operations.
@@ -121,7 +121,6 @@ const operatorsDisplay = () => {
                 equation.push(event.target.dataset.operator);
                 input.textContent += ` ${event.target.dataset.operator} `;
                 operation = event.target.dataset.operator;
-                console.log(equation);
             }
             decimalPoint.disabled = false;
             powerNumbers = false;
@@ -171,9 +170,9 @@ const decimals = () => {
 // Activates when the power button is clicked so that numbers clicked are exponents.
 const powerDisplay = () => {
     powerButton.addEventListener('click', (event) => {
-        if ( (!input.textContent) 
-            || input.textContent[input.textContent.length - 1] === ' ' 
-            || input.textContent[input.textContent.length - 1] === rootButtons[0].dataset.root 
+        if ( (!input.textContent)
+            || input.textContent[input.textContent.length - 1] === ' '
+            || input.textContent[input.textContent.length - 1] === rootButtons[0].dataset.root
             || input.textContent[input.textContent.length - 1] === rootButtons[1].dataset.root){
 
             input.textContent += '0';
@@ -207,7 +206,7 @@ const addPower = (span) => {
     })
 
     rootButtons.forEach(button => {
-        button.addEventListener('click', (event) => {  
+        button.addEventListener('click', (event) => {
             if (powerNumbers === true){
                 span.textContent += event.target.dataset.root;
                 rootButtons[0].disabled = true; rootButtons[1].disabled = true;
@@ -238,13 +237,91 @@ const rootDisplay = () => {
 const solvePowers = (result) => {
     // Goes through the equations array an checks for any ^ so powers can be solved first
     for (let i in equation){
-        if (equation[i].includes('^')){
+        if (typeof equation[i] == "string" && equation[i].indexOf('^') > -1){
             const caratIndex = equation[i].indexOf('^');
             const base = equation[i].slice(0, caratIndex);
             const exponent = equation[i].slice(caratIndex + 1);
             result = operate(power, parseFloat(base), parseFloat(exponent));
             result = result.toString();
             equation.splice(i, 1, result);
+        }
+    }
+}
+
+
+const solveEquation = (result, operator) => {
+// Variable checks if there is a multiplication or division symbol in the equation
+let multOrDiv = true;
+
+// Solves the calculation in the equations array according to operator precedence
+while (equation.length !== 1){
+    if (multOrDiv === true){
+        for (let i = 1; i < equation.length; i += 2){
+            if (equation[i] === '/' || equation[i] === 'x'){
+                operator = (equation[i] === '/') ? division : multiplication;
+                result = operate(operator, parseFloat(equation[i - 1]), parseFloat(equation[i + 1]));
+                result = result.toString();
+                equation.splice(i-1, 3, result);
+                multOrDiv = true;
+                break;
+                }
+            else{
+                multOrDiv = false;
+                }
+            }
+            }
+
+            if (multOrDiv === false){
+                for (let i = 1; i < equation.length; i += 2){
+                    if (equation[i] === '+' || equation[i] === '-'){
+                        operator = (equation[i] === '+') ? addition : subtraction;
+                        result = operate(operator, parseFloat(equation[i - 1]), parseFloat(equation[i + 1]));
+                        result = result.toString();
+                        equation.splice(i-1, 3, result);
+                        break;
+                        }
+                    }
+                }
+            }
+}
+
+const solveRoot = (result) => {
+    const square = rootButtons[0].dataset.root;
+    const cube = rootButtons[1].dataset.root;
+    let root;
+    let rootIndex;
+    for (let i in equation){
+        // Solves case when there is just a root and a base i.e 3**0.5
+        if ((equation[i][0] === square
+            || equation[i][0] === cube)
+            && equation[i].includes('^') === false){
+            
+            if (equation[i][0] === square) result = squareRoot(parseFloat(equation[i].slice(1)));
+            
+            else result = cubeRoot(parseFloat(equation[i].slice(1)));
+
+            equation[i] = result.toString();
+        }
+        
+        // Solves cases where there is a multiple in front of the root i.e 3(3**0.5)
+        else if (isNaN(equation[i][0]) === false 
+            && equation[i].includes('^') === false
+            && (equation[i].includes(square) || equation[i].includes(cube))){
+                
+                if (equation[i].includes(square)){
+                    rootIndex = equation[i].indexOf(square);
+                    root = squareRoot;
+                } 
+                else {
+                    rootIndex = equation[i].indexOf(cube);
+                    root = cubeRoot;
+                }
+
+                const multiple = equation[i].slice(0, rootIndex);
+                result = root(parseFloat(equation[i].slice(rootIndex + 1)));
+                result = parseFloat(multiple) * result;
+                equation[i] = result.toString();
+
         }
     }
 }
@@ -256,45 +333,15 @@ const equals = () => {
             const operatorIndex = input.textContent.lastIndexOf(operation) + 2;
             const lastNumber = input.textContent.substring(operatorIndex);
             equation.push(lastNumber);
-            let operator;
             let result;
-            // Variable checks if there is a multiplication or division symbol in the equation
-            let multOrDiv = true; 
+            let operator;
+            
+            solveRoot(result);
 
             solvePowers(result);
 
-            // Solves the calculation in the equations array according to operator precedence
-            while (equation.length !== 1){
-                if (multOrDiv === true){
-                    for (let i = 1; i < equation.length; i += 2){
-                        if (equation[i] === '/' || equation[i] === 'x'){
-                            operator = (equation[i] === '/') ? division : multiplication;
+            solveEquation(result, operator);
 
-                            result = operate(operator, parseFloat(equation[i - 1]), parseFloat(equation[i + 1]));
-                            result = result.toString();
-                            equation.splice(i-1, 3, result);
-                            multOrDiv = true;
-                            break;
-                        }
-                        else{
-                            multOrDiv = false;
-                        }
-                    }
-                }
-
-                if (multOrDiv === false){
-                    for (let i = 1; i < equation.length; i += 2){
-                        if (equation[i] === '+' || equation[i] === '-'){
-                            operator = (equation[i] === '+') ? addition : subtraction;
-
-                            result = operate(operator, parseFloat(equation[i - 1]), parseFloat(equation[i + 1]));
-                            result = result.toString();
-                            equation.splice(i-1, 3, result);
-                            break;
-                        }
-                    }
-                }
-            }
             output.textContent = input.textContent + ' =';
             //Sets decimals to 4 decimal places and deals with floating point errors.
             input.textContent = parseFloat(parseFloat(equation[0]).toFixed(4)).toString();
@@ -307,8 +354,8 @@ const equals = () => {
         })
 }
 
-const solveEquation = () => {
-    
+const solve = () => {
+
     displayNumber();
     decimals();
     operatorsDisplay();
@@ -318,5 +365,5 @@ const solveEquation = () => {
 }
 
 
-solveEquation();
+solve();
 
